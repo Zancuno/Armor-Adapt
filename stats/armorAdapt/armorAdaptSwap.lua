6164c801a5dc8436002c56336a08fab8fd18e780
Zancuno <44991385+Zancuno@users.noreply.github.com>
2022-06-10 02:48:01 -0400

    Initial Resource Branch Emptying

--- stats/armorAdapt/armorAdaptSwap.lua
+++
@@ -1,7 +0,0 @@
-function init()
-	world.sendEntityMessage(entity.id(), "applyStatusEffect", "armorAdapt_resetTrigger")
-end
-
-function uninit()
-	world.sendEntityMessage(entity.id(), "applyStatusEffect", "armorAdapt_resetTrigger")
-end
